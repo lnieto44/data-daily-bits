e1c4c00952a67b21941276b5536328092ba667b2
Guillermmo Nieto <lnieto44@itfip.edu.co>
2022-03-31 13:26:39 -0500

    cambios

---
+++ data/data.js
@@ -0,0 +1,72 @@
+{
+    "Registro" [
+        {
+            "id": 1,
+            "nombre": "Andres",
+            "apellido": "Parra",
+            "imagen": "imagen",
+            "correo": "abdrespa@gmail.com",
+            "clave": "123456"
+        },
+        {
+            "id": 2,
+            "nombre": "Camila",
+            "apellido": "Buitrago",
+            "imagen": "imagen",
+            "correo": "cabuitrago@gmail.com",
+            "clave": "123456"
+        },
+        {
+            "id": 3,
+            "nombre": "Marcela",
+            "apellido": "Pacheco",
+            "imagen": "imagen",
+            "correo": "marcePacheco@gmail.com",
+            "clave": "123456"
+        },
+        {
+            "id": 4,
+            "nombre": "Diana",
+            "apellido": "Moreno",
+            "imagen": "imagen",
+            "correo": "diana_moreno@gmail.com",
+            "clave": "123456"
+        },
+        {
+            "id": 5,
+            "nombre": "Luis",
+            "apellido": "Rojas",
+            "imagen": "imagen",
+            "correo": "lrojas@gmail.com",
+            "clave": "123456"
+        }
+    ],
+    "Usuarios" [
+        {
+          "url": "https://res.cloudinary.com/duzf4vfki/image/upload/v1637622238/iconos/2_aywkbg.png",
+          "name": "Carlos",
+          "email": "carlos@gmail.com",
+          "description": "Lorem Ipsum",
+          "id": 2
+        }
+      ], 
+      "Estadistica" [
+        {
+            "totalResponse": 20,
+            "successResponses": 15,
+            "failedResponses": 5,
+            "hours": 2,
+            "userId": 1,
+            "id": 1
+          },
+          {
+            "totalResponse": 20,
+            "successResponses": 15,
+            "failedResponses": 5,
+            "hours": 2,
+            "userId": 2,
+            "id": 2
+          }
+      ]
+      
+}
